18cb7a69445f331b1db7e5134384efa5a6980205
yuvaneshkm <yuvaneshkm05@gmail.com>
2024-06-11 17:40:26 +0530

    division_done

--- demo1.py
+++ demo1.py
@@ -7,3 +7,8 @@ print(f'The addition of a and b is {a+b}')
 c = 50
 d = 60
 print(f'The multiplication of c and d is {c*d}')
+
+# divide two numbers:
+e = 50
+f = 10
+print(f'The division of e and f is {e/f}')
